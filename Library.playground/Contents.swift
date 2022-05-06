c8f9e1d8f135c54b9676d3e1c57d9b8db1c44e30
Mattia Fasoli <fasoxy@gmail.com>
2022-05-06 15:33:38 +0200

    Create Class Author e Class Work

--- Library.playground/Contents.swift
+++ Library.playground/Contents.swift
@@ -1,1 +1,26 @@
+class Author {
+    var name: String
+    var surname: String
+    var yob: Int?
+    var pseudonym: String?
+    var works: Array<Work>
+    
+    init(name: String, surname: String) {
+        self.name = name
+        self.surname = surname
+        works = [Work]()
+    }
+}
+
+class Work {
+    var title: String
+    var year: Int
+    var author: Author
+    
+    init(withTitle title: String, createdBy author: Author, createdInYear year: Int) {
+        self.title = title
+        self.year = year
+        self.author = author
+    }
+}
 
