5ec96a38a5fd35a0b18efeb559d55c63df8ae12e
Mattia Fasoli <fasoxy@gmail.com>
2022-05-09 23:09:34 +0200

    New Commit

--- Library.playground/Contents.swift
+++ Library.playground/Contents.swift
@@ -10,13 +10,13 @@ struct Author: CustomStringConvertible {
     var pseudonym: String?
     var works: Array<Work>
     
-    init(name: String, surname: String, yob: Int?, pseudonym: String?) {
-        self.name = name
-        self.surname = surname
-        self.yob = yob
-        self.pseudonym = pseudonym
-        works = [Work]()
-    }
+//    init(name: String, surname: String, yob: Int?, pseudonym: String?) {
+//        self.name = name
+//        self.surname = surname
+//        self.yob = yob
+//        self.pseudonym = pseudonym
+//        works = [Work]()
+//    }
     
     var description: String {
         return "\(name) \(surname) \(yob!)"
@@ -26,16 +26,15 @@ struct Author: CustomStringConvertible {
 class Work: CustomStringConvertible {
     var title: String
     var year: Int
-    var author: Author
+    var author: Author?
     
-    init(withTitle title: String, createdBy author: Author, createdInYear year: Int) {
+    init(withTitle title: String, createdInYear year: Int) {
         self.title = title
         self.year = year
-        self.author = author
     }
     
     var description: String {
-        return "\(title) \(author) \(year)"
+        return "\(title) \(year)"
     }
 }
 
@@ -164,9 +163,18 @@ let url = "https://ewserver.di.unimi.it/mobicomp/esercizi/library.json"
 guard let dataUrl = URL(string: url) else {
     fatalError("Wrong Url Format")
 }
+struct Model {
+    let authors: [Author] = []
+}
 
 struct AuthorSupportHelperModel {
     let author: Author
+    let supports: [Item]
+}
+
+struct Library {
+    let authors: [Author]
+    let supports: [Item]
 }
 
 let session = URLSession.shared
@@ -189,24 +197,78 @@ let task = session.dataTask(with: request, completionHandler: {data, response, e
         return
     }
     
-    let authorItem = (dataParsed["authors"] as? [[String:Any]])?
+    let authorsSupports = (dataParsed["authors"] as? [[String:Any]])?
         .map{(authorDict:[String:Any]) -> AuthorSupportHelperModel in
+            
+            var works = [Work]()
+            var supports = [Item]()
+            
+            if let workDicts = authorDict["works"] as? [[String:Any]] {
+                workDicts.forEach {
+                    let workType = WorkType(rawValue: $0["type"] as! String)!
+                    switch workType {
+                    case .book:
+                        works.append(Book(withTitle: $0["title"] as! String, createdInYear: $0["year"] as! Int))
+                    case .video:
+                        works.append(Video(withTitle: $0["title"] as! String, createdInYear: $0["year"] as! Int))
+                    case .music:
+                        works.append(Music(withTitle: $0["title"] as! String, createdInYear: $0["year"] as! Int))
+                    }
+                    if let supportDicts = $0["supports"] as? [[String:Any]] {
+                        supportDicts.forEach { supportDict in
+                            let associatedWork = works.last!
+                            switch associatedWork {
+                            case is Book:
+                                let numberOfSameSupports = supportDict["item"] as? Int ?? 1
+                                for _ in stride(from: 0, to: numberOfSameSupports, by: 1) {
+                                    supports.append(((supportDict["digital"] as! Bool) == true) ?
+                                        Ebook(forBook: associatedWork as! Book, withSize: (supportDict["size"] as! NSNumber).floatValue) :
+                                        Volume(forBook: associatedWork as! Book, pubishedBy: supportDict["publisher"] as! String, withPosition: Position(shelfNumber: Int(supportDict["shelfNumber"] as! String)!, shelfPosition: Int(supportDict["shelfPosition"] as! String)!))
+                                    )
+                                }
+                            case is Video:
+                                let numberOfSameSupports = supportDict["item"] as? Int ?? 1
+                                for _ in stride(from: 0, to: numberOfSameSupports, by: 1) {
+                                    supports.append(Dvd(forVideo: associatedWork as! Video, dvdNumber: supportDict["number"] as! Int, withPosition: Position(shelfNumber: Int(supportDict["shelfNumber"] as! String)!, shelfPosition: Int(supportDict["shelfPosition"] as! String)!)))
+                                }
+                            case is Music:
+                                let numberOfSameSupports = supportDict["item"] as? Int ?? 1
+                                for _ in stride(from: 0, to: numberOfSameSupports, by: 1) {
+                                    supports.append(Cd(forMusic: associatedWork as! Music, withTracksNumber: supportDict["tracks"] as! Int, withPosition: Position(shelfNumber: Int(supportDict["shelfNumber"] as! String)!, shelfPosition: Int(supportDict["shelfPosition"] as! String)!)))
+                                }
+                            default:
+                                break
+                            }
+                        }
+                    }
+                }
+            }
             
             let author = Author(
                 name: authorDict["name"] as! String,
                 surname: authorDict["surname"] as! String,
                 yob: authorDict["yob"] as? Int,
-                pseudonym: authorDict["pseudonym"] as? String
+                pseudonym: authorDict["pseudonym"] as? String,
+                works: works
             )
             
-            if let yob = author.yob, let pseudonym = author.pseudonym  {
-                print("\(author.name) \(author.surname) \(yob) \(pseudonym)")
-            }
+            author.works.forEach{$0.author = author}
             
-            return AuthorSupportHelperModel(author: author)
+            return AuthorSupportHelperModel(author: author, supports: supports)
         }
     
+    guard let authorsSupportsRecords = authorsSupports else {return}
+
+    let allAuthors = authorsSupportsRecords
+        .compactMap{$0.author}
+    let allSupports = authorsSupportsRecords
+        .compactMap{$0.supports}
+        .flatMap{$0}
+
+    let myLibrary = Library(authors: allAuthors, supports: allSupports)
 })
 
 task.resume()
+
+
 
